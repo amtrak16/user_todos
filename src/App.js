d7a0c31d76b16ff9ae836d30ec288c5a435e9652
Tony Lauf <LAU5619@Tonys-MacBook-Pro.local>
2018-02-06 22:33:07 -0600

    got users adding and selecting

--- src/App.js
+++ src/App.js
@@ -7,56 +7,7 @@ import {
   NavLink
 } from 'react-router-dom'
 
-class User {
-  constructor(
-    name,
-  ) {
-    this.name = name;
-  }
-}
-const initialState = {
-  users: [
-    new User('Bobby'),
-    new User('Henry'),
-    new User('Sofie'),
-    new User('Miranda'),
-    new User('Jerome'),
-    new User('Millie')
-  ],
-  nameVal: '',
-  nameMsg: '',
-  nameErr: false,
-  nameSelect: false,
-  disableSbmBtn: true
-}
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = initialState
-    this.onNameIn = this.onNameIn.bind(this)
-    this.onNameAdd = this.onNameAdd.bind(this)
-
-  }
-
-  onNameIn({ target }) {
-    if (target.value.length === 0) {
-      this.setState({ nameVal: target.value, nameErr: true, nameMsg: 'Please enter a value for the users name.', nameSelect: false, nameRspMsg: '', disableSbmBtn: true })
-    } else {
-      this.setState({ nameVal: target.value, nameErr: false, nameMsg: '', nameSelect: false })
-    }
-  }
-
-  onNameAdd({ target }) {
-    let personSelected = []
-    this.state.users.forEach((user) => {
-      if (user.id.indexOf(target.id) === -1) { return; }
-      personSelected.push(user)
-      this.setState({ nameSelect: true, nameSelected: user })
-    })
-  }
-
   render() {
     return (
       <div className="App">
@@ -68,8 +19,7 @@ class App extends Component {
               <li className="heading-nav-entry"><NavLink exact to="/todos" activeClassName="active" activeStyle={{ padding: '5px', backgroundColor: 'lightgrey', borderRadius: '5px' }}>Todos</NavLink></li>
             </ul>
             <Route exact path="/" component={Home} />
-            <Route path="/users" render={(props) => (<Users {...props} state={this.state} />
-            )} />
+            <Route path="/users" render={(props) => (<Users {...props} />)} /> 
             <Route path="/todos" component={Todos} />
           </div>
         </Router>
@@ -104,54 +54,113 @@ class Home extends Component {
   }
 }
 
-const Users = (props) => {
-    const {match} = props
-    return (
+class User {
+  constructor(
+    name,
+  ) {
+    this.name = name;
+  }
+}
+const initialState = {
+  users: [
+    new User('Bobby'),
+    new User('Henry'),
+    new User('Sofie'),
+    new User('Miranda'),
+    new User('Jerome'),
+    new User('Millie')
+  ],
+  nameVal: '',
+  nameMsg: '',
+  nameErr: false,
+  nameSelect: false,
+  disableSbmBtn: true
+}
+
+class Users extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props)
+
+    this.state = initialState
+    this.onNameIn = this.onNameIn.bind(this)
+    this.onNameAdd = this.onNameAdd.bind(this)
+    // this.onNameClick = this.onNameClick.bind(this)
+  }
+
+  onNameIn({ target }) {
+    if (target.value.length === 0) {
+      this.setState({ nameVal: target.value, nameErr: true, nameMsg: 'Please enter a user name.', nameSelect: false, nameRspMsg: '', disableSbmBtn: true })
+    } else {
+      this.setState({ nameVal: target.value, nameErr: false, nameMsg: '', nameSelect: false, disableSbmBtn: false })
+    }
+  }
+
+  onNameAdd({ target }) {
+    let newUsers = this.state.users.slice(0)
+    let newUser = new User(this.state.nameVal)
+    newUsers.push(newUser)
+    console.log(this.state.nameVal, newUsers)
+    this.setState({ users: newUsers })
+  }
+
+  // onNameClick({ target }) {
+  //   let personSelected = []
+  //   this.state.users.forEach((user) => {
+  //     if (user.id.indexOf(target.value) === -1) { return; }
+  //     personSelected.push(user)
+  //     this.setState({ nameSelect: true, nameSelected: user })
+  //   })
+  // }
+
+  render() {
+  return (
       <div>
         <div class="row">
-          <h2 class="small-2 columns">Add User</h2>
-          <div class="small-5 columns md-text-field with-floating-label icon-left">
-            <input type="search" id="name_in" placeholder='Add User' value={props.state.nameVal} onChange={props.onNameIn} />
+          <h3 class="small-2 columns">Add User</h3>
+          <div class="small-2 columns md-text-field with-floating-label icon-left">
+            <input type="search" id="name_in" placeholder='Add User' value={this.state.nameVal} onChange={this.onNameIn} />
             <label for="name_in"></label>
-            <span class="error">{props.state.nameMsg}</span>
+            <span class="error">{this.state.nameMsg}</span>
           </div>
-          <div className="small-2 columns" >&nbsp;</div>
-          <button className="button btn-cta small-4 columns" disabled={props.state.disableSbmBtn} onClick={props.onNameAdd}>Add User</button>
-          <div className="small-2 columns" >&nbsp;</div>
+        <button className="button btn-cta small-2 columns" disabled={this.state.disableSbmBtn} onClick={this.onNameAdd}>Add User</button>
+          <div className="small-6 columns" >&nbsp;</div>
         </div>
+        <div class="row">
+          <div class="small-2 columns"></div>
+          <div class="small-2 columns">
+          </div>
+          <div class="small-8 columns"></div>
+        </div>
+
         <ul class="tabs vertical">
-          <li className="tab-title small-1 columns"><NavLink exact to="/users/1" activeClassName="active" activeStyle={{ padding: '5px', backgroundColor: 'lightgrey', borderRadius: '5px' }}>User 1</NavLink></li>
+          {this.state.users.map((user, idx) => {
+            return(
+              <li className="tab-title small-1 columns" key={idx} ><NavLink exact to={`/users/${user.name}`} activeClassName="active" activeStyle={{ padding: '5px', backgroundColor: 'lightgrey', borderRadius: '5px' }}>{user.name}</NavLink></li>
+            )
+          })}
+        {/* onClick={this.onNameClick} */}
+
+        {/* <NavLink exact to="/users/:id" activeClassName="active" activeStyle={{ padding: '5px', backgroundColor: 'lightgrey', borderRadius: '5px' }}>{user.name}</NavLink> */}
+
+          {/* <li className="tab-title small-1 columns"><NavLink exact to="/users/1" activeClassName="active" activeStyle={{ padding: '5px', backgroundColor: 'lightgrey', borderRadius: '5px' }}>User 1</NavLink></li>
           <li className="tab-title small-1 columns"><NavLink exact to="/users/2" activeClassName="active" activeStyle={{ padding: '5px', backgroundColor: 'lightgrey', borderRadius: '5px' }}>User 2</NavLink></li>
           <li className="tab-title small-1 columns"><NavLink exact to="/users/3" activeClassName="active" activeStyle={{ padding: '5px', backgroundColor: 'lightgrey', borderRadius: '5px' }}>User 3</NavLink></li>
-          <li className="tab-title small-1 columns"><NavLink exact to="/users/4" activeClassName="active" activeStyle={{ padding: '5px', backgroundColor: 'lightgrey', borderRadius: '5px' }}>User 4</NavLink></li>
+          <li className="tab-title small-1 columns"><NavLink exact to="/users/4" activeClassName="active" activeStyle={{ padding: '5px', backgroundColor: 'lightgrey', borderRadius: '5px' }}>User 4</NavLink></li> */}
         </ul>
         <Route exact path="/users/:id" component={UserDetail} />
       </div>
     )
   }
+}
 
 const UserDetail = (props) => {
   console.log(props)
   return (
-    <div className="small-10 columns" >
+    // <hr></hr>
+    <div className="small-12 columns" >
       <div className="card">
-        <h1>User {props.match.params.id}</h1>
-        {props.match.params.id == 1 &&
-          <h3>Now is the time for all good men to come to the aid of their country to defend its people and treasure against all comers, great and small, internal and external, in plain sight and covert.</h3>
-        }
-        {props.match.params.id == 2 &&
-          <h3>What we really are matters more than what other people think of us. Jawaharlal Nehru
-Read more at: https://www.brainyquote.com/quotes/jawaharlal_nehru_163872.</h3>
-        }
-        {props.match.params.id == 3 &&
-          <h3>I didn't attend the funeral, but I sent a nice letter saying I approved of it. Mark Twain
-Read more at: https://www.brainyquote.com/quotes/mark_twain_100376</h3>
-        }
-        {props.match.params.id == 4 &&
-          <h3>Love is a better teacher than duty. Albert Einstein
-Read more at: https://www.brainyquote.com/quotes/albert_einstein_148793.</h3>
-        }
-
+        <h1 class="text-center">User {props.match.params.id}</h1>
       </div >
     </div>)
 }
